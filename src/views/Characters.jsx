9fc6f0f1ce2e94071581c86e31e28ec9bf6ba921
Edgar Rincon <rinconaedgar@gmail.com>
2021-11-10 18:01:27 -0300

    done

--- src/views/Characters.jsx
+++ src/views/Characters.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { Context } from "../store/appContext";
 
 const Characters = (props) => {
-    const {store, actions} = useContext(Context);
-    
+    const { store, actions } = useContext(Context);
+
+
+
     const clickHandler = (characterName) => {
         actions.addFavorites(characterName);
     }
@@ -12,18 +14,19 @@ const Characters = (props) => {
     const charsStarWars = store.people.map((character) => {
         return (<div className="col-4 py-2 mt-5">
             <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{character.name}</h5>
-                <p className="card-text">Character's height is {character.height}</p>
-                <i className="fas fa-heart" onClick={() => clickHandler(character.name)}>Click to add to favorites</i>
-            </div>
-        </div></div>)
+                <div className="card-body">
+                    <h5 className="card-title">{character.name}</h5>
+                    <p className="card-text">Character's height is {character.height}</p>
+                    <i className="fas fa-heart" onClick={() => clickHandler(character.name)}>Click to add to favorites</i>
+                </div>
+            </div></div>)
     })
 
     return (
+
         <div className="container mt-5">
             <div className="row">
-                    {charsStarWars}
+                {charsStarWars}
             </div>
         </div>
     )
