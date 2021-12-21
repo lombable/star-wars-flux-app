b04ab217154d59b0c2eccb39d1617cd613f46d92
Edgar Rincon <rinconaedgar@gmail.com>
2021-12-21 13:41:16 -0300

    hotfix

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,4 +1,20 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+
 const Dropdown = () => {
+
+    useEffect(() => {
+        actions.getFavoritesList()
+    }, [])
+
+    const { store, actions } = useContext(Context);
+
+    const favoriteGenerator = store.favorite?.map((favorite, i) => {
+        return (
+            <li key={i}>{favorite}</li>
+            )
+    })
+
     return (
         <div>
             <li className="nav-item dropdown col-12 col-md-auto text-center">
@@ -6,10 +22,9 @@ const Dropdown = () => {
                     Favorites
                 </a>
                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Favorites</a></li>
                     <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <li><a className="dropdown-item" href="#">  {favoriteGenerator}</a></li>
                 </ul>
             </li>
         </div>
